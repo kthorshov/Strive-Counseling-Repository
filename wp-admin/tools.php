a85320db6888f3c5d045fd6c4be102aaf1e43a67
kthorshov <kaleb@strive-counseling.com>
2024-02-24 23:15:00 -0600

    2

---
+++ wp-admin/tools.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://github.com/kthorshov/Strive-Counseling-Repository/wp-login.php?redirect_to=https%3A%2F%2Fstrive-counseling.local%2Fwp-admin%2Ftools.php%3Fsimply_static_page%3D2271&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://github.com/kthorshov/Strive-Counseling-Repository/wp-login.php?redirect_to=https%3A%2F%2Fstrive-counseling.local%2Fwp-admin%2Ftools.php%3Fsimply_static_page%3D2271&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://github.com/kthorshov/Strive-Counseling-Repository/wp-login.php?redirect_to=https%3A%2F%2Fstrive-counseling.local%2Fwp-admin%2Ftools.php%3Fsimply_static_page%3D2271&reauth=1">https://github.com/kthorshov/Strive-Counseling-Repository/wp-login.php?redirect_to=https%3A%2F%2Fstrive-counseling.local%2Fwp-admin%2Ftools.php%3Fsimply_static_page%3D2271&reauth=1</a></p>
+	</body>
+</html>
